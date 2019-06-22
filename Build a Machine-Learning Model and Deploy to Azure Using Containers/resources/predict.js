7d1c77b6518210a81e50217a4183c95653581034
jeffprosise <jeffpro@wintellect.com>
2019-06-22 18:12:35 -0400

    Added client app

---
+++ Build a Machine-Learning Model and Deploy to Azure Using Containers/resources/predict.js
@@ -0,0 +1,23 @@
+$(function () {
+    var url = "http://FQDN/predict";
+
+    // Handle clicks of the Analyze button
+    $("#analyze_button").click(function() {
+        // Get user input
+        var text = $("#input-text").val();
+
+        // Invoke model using REST call
+        $.ajax({
+            type: "GET",
+            url: url + "?text=" + encodeURIComponent(text),
+        }).done(function (data) {
+            showResults(data);
+        }).fail(function(xhr, status, err) {
+            alert(status + " (" + err + ")");
+        });
+    });
+});
+
+function showResults(data) {
+    alert(parseFloat(data).toFixed(4));
+}
